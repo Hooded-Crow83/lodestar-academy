6e388ad3c83c7f9c1359b0098583633a9f03d27f
Hooded-Crow83 <bryan.mccoskey@gmail.com>
2024-08-12 22:14:38 -0400

    initial commit

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -38,3 +38,29 @@ document.getElementById('dark-mode-toggle').addEventListener('click', () => {
 	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
 	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
 });
+const leftOption = document.querySelector("#pricing-1387 #cs-option1-1387");
+        const rightOption = document.querySelector("#pricing-1387 #cs-option2-1387");
+        const toggle = document.querySelector("#pricing-1387 .cs-toggle");
+        const cardGroup = Array.from(document.querySelectorAll('#pricing-1387 .cs-card-group'))
+        // when you click the middle toggle
+        toggle.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.toggle("cs-active");
+            }
+            toggle.classList.toggle("active");
+        });       
+        // when you click the left button option
+        leftOption.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.remove("cs-active");
+            }
+            toggle.classList.remove("active");
+        });    
+        // when you click the right button option
+        rightOption.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.add("cs-active");
+            }
+            toggle.classList.add("active");
+        });
+                                
